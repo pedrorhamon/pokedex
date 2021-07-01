53f82777153a2f29b48f9552d8efb22a534abb56
pedrorhamon <pedrorhamon16@gmail.com>
2021-06-30 21:54:28 -0300

    funcao no ts dos pokemons

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -5,11 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.sass']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent {
+  public pokemons: Pokemon[] = [
+    {
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
+      number: 1,
+      name: 'Bulbasaur',
+      types: [
+        Type.Grass,
+        Type.Poison
+      ]
+    }
+  ];
 
-  constructor() { }
+}
 
-  ngOnInit(): void {
-  }
+interface Pokemon {
+  image: string;
+  number: number;
+  name: string;
+  types: Type[];
+}
 
+enum Type {
+  Grass = 'Grass',
+  Poison = 'Poison'
 }
